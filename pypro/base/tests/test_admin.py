ee595f7decd594effa87b6a0003675de88489bad
Paulo Bueno <paulob.bruno@gmail.com>
2019-08-25 23:40:55 -0300

    1. Use email as log-in 2. Add automatic migrations to heroku close #52 #87

--- pypro/base/tests/test_admin.py
+++ pypro/base/tests/test_admin.py
@@ -19,12 +19,12 @@ def test_user_creation_no_email(email, password):
         assert fake_user.create_user(email=email, password=password)
 
 
-@pytest.mark.parametrize('email, norm_email',
-    [
+@pytest.mark.parametrize('email, norm_email', [
         ('test_user@amaro.com', 'test_user@amaro.com'),
         ('test_user@AMARO.COM', 'test_user@amaro.com'),
         (' TEST_USER@AMARO.COM ', 'TEST_USER@amaro.com'),
-        ('test_user@AMaRO.COM  ', 'test_user@amaro.com')]
+        ('test_user@AMaRO.COM  ', 'test_user@amaro.com')
+    ]
 )
 def test_email_normalize(email, norm_email):
     assert fake_user.normalize_email(email=email) == norm_email
