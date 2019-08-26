a03a5d515410fd5fceb253e003f9ace2ae1cfae3
Paulo Bueno <paulob.bruno@gmail.com>
2019-08-25 23:35:20 -0300

    1. Use email as log-in 2. Add automatic migrations to heroku close #52 #87

--- pypro/base/tests/test_admin.py
+++ pypro/base/tests/test_admin.py
@@ -1,29 +1,30 @@
 import pytest
+
 from pypro.base import models
 
 
 class FakeUser(models.UserManager):
     pass
 
+
 fake_user = FakeUser()
 
-@pytest.mark.parametrize('email, password',
-                        [
-                        (None, 'abc'),
-                        ('', 'abc')]
-                        )
+
+@pytest.mark.parametrize('email, password', [
+    (None, 'abc'),
+    ('', 'abc')]
+)
 def test_user_creation_no_email(email, password):
     with pytest.raises(ValueError):
         assert fake_user.create_user(email=email, password=password)
 
 
 @pytest.mark.parametrize('email, norm_email',
-                         [
-                          ('test_user@amaro.com', 'test_user@amaro.com'),
-                          ('test_user@AMARO.COM', 'test_user@amaro.com'),
-                          (' TEST_USER@AMARO.COM ', 'TEST_USER@amaro.com'),
-                          ('test_user@AMaRO.COM  ', 'test_user@amaro.com')]
-                         )
+    [
+        ('test_user@amaro.com', 'test_user@amaro.com'),
+        ('test_user@AMARO.COM', 'test_user@amaro.com'),
+        (' TEST_USER@AMARO.COM ', 'TEST_USER@amaro.com'),
+        ('test_user@AMaRO.COM  ', 'test_user@amaro.com')]
+)
 def test_email_normalize(email, norm_email):
     assert fake_user.normalize_email(email=email) == norm_email
-
