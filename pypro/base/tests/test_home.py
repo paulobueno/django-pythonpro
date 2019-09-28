3a00df804d4d62dd2e7752e36261d7806ea8c0b9
Paulo Bueno <paulob.bruno@gmail.com>
2019-09-28 18:12:30 -0300

    New template - HTML+CSS + New Title

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,7 +1,12 @@
 from django.test import Client
 
+from pypro.base.django_assertions import assert_contains
+
 
 def test_status_home(client: Client):
-    assert 1 == 1
-    # resp = client.get('/')
-    # assert resp.status_code == 200
+    resp = client.get('/')
+    assert resp.status_code == 200
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Python Project Fun - PPF</title>')
